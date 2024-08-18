359afad5454096336c66d6bdb66a604e12c16492
Volodymyr Machula <machulav@gmail.com>
2024-08-18 22:17:00 +0200

    Update CI

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 module "deploy-plugin-on-aws-lambda" {
-  source = "github.com/connery-io/deploy-plugin-on-aws-lambda?ref=v0.0.2"
+  source = "github.com/connery-io/deploy-plugin-on-aws-lambda?ref=main"
 
   plugin_name    = "fixed-response"
   plugin_version = "v1"
